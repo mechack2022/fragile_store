9fad36f37b0f6c74bedf062eede225b8cd1af923
mechack2022 <dtoms624@gmail.com>
2023-07-25 11:23:08 +0100

    footer added

--- frontend/src/customer/components/footer/Footer.jsx
+++ frontend/src/customer/components/footer/Footer.jsx
@@ -1,133 +1,121 @@
-// import React from "react";
-// import { Button, Grid, Link, Typography } from "@mui/material";
+import React from "react";
 
-// const Footer = () => {
-//   return (
-//     <div className="bg-black">
-//      <Grid
-//         className="text-center text-white mt-10 bg-black"
-//         container
-//         sx={{ bgcolor: "black", color: "white", py: 3 }}
-//       >
-//         <Grid item xs={12} sm={6} md={3}>
-//           <Typography variant="h6" className="pb-5">
-//             Company
-//           </Typography>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Blog
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Press
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Jobs
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Partner
-//             </Button>
-//           </div>
-//         </Grid>
-//         {/* second  */}
-//         <Grid item xs={12} sm={6} md={3}>
-//           <Typography variant="h6" className="pb-5">
-//             Solutins
-//           </Typography>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Marketing
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Commerce
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Insight
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Support
-//             </Button>
-//           </div>
-//         </Grid>
-//         {/* third  */}
+import { Grid, Button, Typography, Link } from '@mui/material';
 
-//         <Grid item xs={12} sm={6} md={3}>
-//           <Typography variant="h6" className="pb5">
-//             Documentation
-//           </Typography>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               API status
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Guides
-//             </Button>
-//           </div>
-//         </Grid>
-//         {/* forth  */}
-//         <Grid item xs={12} sm={6} md={3}>
-//           <Typography variant="h6" className="pb-5">
-//             Legal
-//           </Typography>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Claims
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Privacy
-//             </Button>
-//           </div>
-//           <div>
-//             <Button variant="h6" className="pb-5" >
-//               Terms
-//             </Button>
-//           </div>
-//         </Grid>
-//         {/* text-area  */}
-//         <Grid item className="pt-20" xs={12}>
-//           <Typography align="center" variant="body2" component="2">
-//             &copy; 2023 My company, All rights reserved
-//           </Typography>
-//           <Typography align="center" variant="body2" component="2">
-//             Made with love by Me
-//           </Typography>
-//           <Typography align="center" variant="body2" component="2">
-//             Icon made by{" "}
-//             <Link
-//               href="https://www.freepik.com"
-//               color="inherit"
-//               underline="always"
-//             >
-//               Freepik
-//             </Link>
-//             from{" "}
-//             <Link
-//               href="https://www.flaticon.com/"
-//               color="inherit"
-//               underline="always"
-//             >
-//               www.flaticon.com
-//             </Link>
-//           </Typography>
-//         </Grid>
-//       </Grid>
-//     </div>
-//   );
-// };
+const Footer = () => {
+  return (
+    <div className="bg-black">
+      <Grid container sx={{ color: 'white', py: 3 }}>
+        <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6" sx={{ pb: 5 }}>
+            Company
+          </Typography>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Blog
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Press
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Jobs
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Partner
+            </Button>
+          </div>
+        </Grid>
+        {/* Second */}
+        <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6" sx={{ pb: 5 }}>
+            Solutions
+          </Typography>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Marketing
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Commerce
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Insight
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Support
+            </Button>
+          </div>
+        </Grid>
+        {/* Third */}
+        <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6" sx={{ pb: 5 }}>
+            Documentation
+          </Typography>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              API status
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Guides
+            </Button>
+          </div>
+        </Grid>
+        {/* Fourth */}
+        <Grid item xs={12} sm={6} md={3}>
+          <Typography variant="h6" sx={{ pb: 5 }}>
+            Legal
+          </Typography>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Claims
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Privacy
+            </Button>
+          </div>
+          <div>
+            <Button variant="h6" sx={{ pb: 5 }}>
+              Terms
+            </Button>
+          </div>
+        </Grid>
+        {/* Text area */}
+        <Grid item xs={12}>
+          <Typography align="center" variant="body2">
+            &copy; 2023 My Company, All rights reserved
+          </Typography>
+          <Typography align="center" variant="body2">
+            Made with love by Me
+          </Typography>
+          <Typography align="center" variant="body2">
+            Icon made by{' '}
+            <Link href="https://www.freepik.com" color="inherit" underline="always">
+              Freepik
+            </Link>{' '}
+            from{' '}
+            <Link href="https://www.flaticon.com/" color="inherit" underline="always">
+              www.flaticon.com
+            </Link>
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 
-// export default Footer;
+export default Footer;
