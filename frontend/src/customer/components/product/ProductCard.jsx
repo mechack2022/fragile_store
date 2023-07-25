e10af635f3494a54c8ac973bd8b144cffe758979
mechack2022 <dtoms624@gmail.com>
2023-07-25 11:13:07 +0100

    productCard modified

--- frontend/src/customer/components/product/ProductCard.jsx
+++ frontend/src/customer/components/product/ProductCard.jsx
@@ -4,7 +4,7 @@ import './ProductCard.css'
 const ProductCard = ({product}) => {
   return (
    <div className="productCard w-[15rem] transition-all cursor-pointer m-3">
-    <div className="h-[20rem">
+    <div className="h-[20rem]">
         <img src={product.imageUrl} alt="" className="h-full w-full object-cover object-left-top" />
     </div>
     <div className="textPart bg-white p-3">
