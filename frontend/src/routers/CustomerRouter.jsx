f9ce2119f0a943ee6331c27ac2c138c669bdf6f4
mechack2022 <dtoms624@gmail.com>
2023-09-07 02:27:38 +0100

    router folder added

---
+++ frontend/src/routers/CustomerRouter.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Cart from "../customer/components/cart/Cart";
+import Footer from "../customer/components/footer/Footer";
+import Navigation from "../customer/components/naviagation/Navigation";
+import Product from "../customer/components/product/Product";
+import ProductDetails from "../customer/components/productDetails/ProductDetails";
+import HomePages from "../customer/pages/homePages/HomePages";
+import Checkout from "../customer/components/checkout/checkout";
+import Order from "../customer/components/order/Order";
+import OrderDetails from "../customer/components/order/OrderDetails";
+
+const CustomerRouter = () => {
+  return (
+    <div>
+      <div className="">
+        <Navigation />
+      </div>
+      <Routes>
+        <Route path="/" element={<HomePages />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/:levelOne/:levelTwo/:levelThree" element={<Product />} />
+        <Route path="/product/:productId" element={<ProductDetails />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/account/order" element={<Order />} />
+        <Route path="/account/order/:orderId" element={<OrderDetails />} />
+      </Routes>
+
+      <div className="">
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default CustomerRouter;
