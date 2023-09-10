4ad07bab8021e144e9564404ea2b7eaa423a7ab2
mechack2022 <dtoms624@gmail.com>
2023-09-10 08:04:04 +0100

    cartReducer and sloce added

--- frontend/src/state/store.js
+++ frontend/src/state/store.js
@@ -1,9 +1,13 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import thunk from 'redux-thunk';
 import { AuthReducer } from "./auth/Reducer.js";
+import { cartReducer } from "./cart/Reducer.js";
+import { customerProductReducer } from "./product/Reducer.js";
 
 const rootReducers = combineReducers({
-   authReducer:AuthReducer
+   authReducer:AuthReducer,
+   customerProductReducer: customerProductReducer,
+   cartReducer: cartReducer
 })
 
 export const Store = legacy_createStore(rootReducers, applyMiddleware(thunk))
